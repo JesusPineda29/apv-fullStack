a4223b44efc8ec38f943496081df563bbec0e472
JesusPineda29 <jesuspineda1429@gmail.com>
2025-04-24 12:43:57 -0600

    mejorando la autenticación usando JWT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AuthLayout from './layout/AuthLayout'
 import Login from './paginas/Login'
 import Registrar from './paginas/Registrar'
@@ -6,26 +6,31 @@ import OlvidePassword from './paginas/OlvidePassword'
 import ConfirmarCuenta from './paginas/ConfirmarCuenta'
 import NuevoPassword from './paginas/NuevoPassword'
 
+import { AuthProvider } from './context/AuthProvider'
+
 function App() {
 
 
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<AuthLayout />}>
-                <Route index element={<Login />} />
-                <Route path='registrar' element={<Registrar/>} />
-                <Route path='olvide-password' element={<OlvidePassword/>} />
-                <Route path='olvide-password/:token' element={<NuevoPassword/>} />
-                <Route path='confirmar/:id' element={<ConfirmarCuenta/>} />
-            </Route>
+        <AuthProvider>
+            <Routes>
+                <Route path='/' element={<AuthLayout />}>
+                    <Route index element={<Login />} />
+                    <Route path='registrar' element={<Registrar />} />
+                    <Route path='olvide-password' element={<OlvidePassword />} />
+                    <Route path='olvide-password/:token' element={<NuevoPassword />} />
+                    <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+                </Route>
 
 
-
-        </Routes>
+                
+            </Routes>
+        </AuthProvider>
     </BrowserRouter>
   )
 }
+
 
 export default App
 
--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -7,6 +7,8 @@ const OlvidePassword = () => {
   const [email, setEmail] = useState('');
   const [alerta, setAlerta] = useState({})
 
+
+
   const handleSubmit = async (e) => {
     e.preventDefault()
 
